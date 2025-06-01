c9ba8af77194bb6798826aa266368ec0b3c48d71
Your Name <you@example.com>
2025-06-01 00:40:12 -0400

    updated changes

--- venv/src/load_data.py
+++ venv/src/load_data.py
@@ -4,6 +4,18 @@ import numpy as np
 
 df = pd.read_csv("uniqlo_sales_dataset.csv")
 
-df.head()       # shows first 5 rows of data
-df.info()       # displays summary of data types, non-null values and memory usage
-df.describe()   # gives statistical summaries
+df.head()                               # shows first 5 rows of data
+df.info()                               # displays summary of data types, non-null values and memory usage
+df.describe()                           # gives statistical summaries
+
+cleaned_columns = []                    # Create an empty list to hold the cleaned column names
+
+# Loop through each column name
+for col in df.columns:
+    col = col.strip()                   # Remove leading/trailing spaces
+    col = col.lower()                   # Convert to lowercase
+    col = col.replace(' ', '_')         # Replace spaces with underscores
+    cleaned_columns.append(col)         # Add to new list
+
+
+df.columns = cleaned_columns            # Assign cleaned column names back to the DataFrame
